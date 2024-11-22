fe391cbd5c5c5fb60bae5afb44b85cfd8cbf2ccb
Shyam Sundar Bhushan <catchshyam14@gmail.com>
2024-11-22 17:30:50 +0530

    initializing spring boot project

---
+++ src/main/java/com/shyam/SpringGitHubDemo/SpringGitHubDemoApplication.java
@@ -0,0 +1,14 @@
+package com.shyam.SpringGitHubDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitHubDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringGitHubDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shyam/SpringGitHubDemo/SpringGitHubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shyam.SpringGitHubDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitHubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
